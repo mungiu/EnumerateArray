bde79c5350a8d7060da132474157c31690978bd6
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-08-18 00:11:16 +0200

    Tutorial Checkpoint Completed

--- EnumerateArray/Program.cs
+++ EnumerateArray/Program.cs
@@ -10,6 +10,68 @@ namespace EnumerateArray
     {
         static void Main(string[] args)
         {
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+
+            Console.WriteLine();
+            MakeDaysPlural(daysOfWeek);
+
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+
+            Console.WriteLine();
+            MakeDaysPlural2(daysOfWeek);
+
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+            //StringBuilder sb = new StringBuilder();
+            //for (int i = 0; i < daysOfWeek.Length; i++)
+            //{
+            //    sb.Append(daysOfWeek[i]);
+            //    if (i < daysOfWeek.Length - 2)
+            //        sb.Append(", ");
+            //    else if (i == daysOfWeek.Length - 2)
+            //        sb.Append(" and ");
+            //}
+            //Console.WriteLine(sb);
         }
+
+
+        static void MakeDaysPlural(string[] daysOfWeek)
+        {
+            for (int i = 0; i < daysOfWeek.Length; i++)
+            {
+                string day = daysOfWeek[i];
+                daysOfWeek[i] = day + "s";
+            }
+        }
+
+        /* This will not work because we are merely creating a copy of
+         * a reference type and not changing the original variable
+        static void MakeDaysPlural2(string[] daysOfWeek)
+        {
+            foreach (string day in daysOfWeek)
+            {
+                string day = day + "s";
+            }
+        }
+        */
     }
 }
